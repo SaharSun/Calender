c17aa4bb6a4c8b473bdf068ecb9dfab211170d3b
sahar <sahar8013@gmail.com>
2024-02-11 20:45:42 +0330

    add change date

--- full_Calender/changing/changing.go
+++ full_Calender/changing/changing.go
@@ -8,6 +8,7 @@ type date struct {
 	day   int
 }
 
+// changing to jalali 
 func (d *date) gregorian_to_jalali() (int, int, int) {
 	var jy, jm, jd, gy2, days int
 	var g_d_m = [12]int{0, 31, 59, 90, 120, 151, 181, 212, 243, 273, 304, 334}
@@ -34,14 +35,64 @@ func (d *date) gregorian_to_jalali() (int, int, int) {
 	}
 	return jy, jm, jd
 }
-func changed() (int, int, int) {
+func Changed_to_jalali(y, m, d int) (int, int, int) {
 	w := &date{
-		year:  2024,
-		month: 2,
-		day:   6,
+		year:  y,
+		month: m,
+		day:   d,
 	}
 
 	jy, jm, jd := w.gregorian_to_jalali()
 	return jy, jm, jd
+}
 
+
+// changing to gregorian 
+func (d *date) jalali_to_gregorian() ( int, int, int) {
+	var gy, gm, gd, days int
+	var sal_a = [13]int{0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31}
+	d.year += 1595
+	days = -355668 + (365 * d.year) + ((d.year / 33) * 8) + (((d.year % 33) + 3) / 4) + d.day
+	if (d.month < 7) {
+	  days += (d.month - 1) * 31
+	} else {
+	  days += ((d.month - 7) * 30) + 186
+	}
+	gy = 400 * (days / 146097)
+	days %= 146097
+	if (days > 36524) {
+	  days--
+	  gy += 100 * (days / 36524)
+	  days %= 36524
+	  if (days >= 365) {
+		days++
+	  }
+	}
+	gy += 4 * (days / 1461)
+	days %= 1461
+	if (days > 365) {
+	  gy += (days - 1) / 365
+	  days = (days - 1) % 365
+	}
+	gd = days + 1
+	if ((gy % 4 == 0 && gy % 100 != 0) || (gy % 400 == 0)) {
+	  sal_a[2] = 29
+	}
+	gm = 0
+	for (gm < 13 && gd > sal_a[gm]) {
+	  gd -= sal_a[gm]
+	  gm++
+	}
+	return gy, gm, gd
+  }
+  
+  func Changed_to_gregorian(y, m, d int) (int, int, int) {
+	w := &date{
+		year:  y,
+		month: m,
+		day:   d,
+	}
+
+	jy, jm, jd := w.jalali_to_gregorian()
+	return jy, jm, jd
 }
--- full_Calender/main.go
+++ full_Calender/main.go
@@ -1,10 +1,8 @@
 package main
 
 import (
-	"Fuul_Calender/changing/changed"
+	"Fuul_Calender/changing"
 	"fmt"
-
-	
 )
 
 func main() {
@@ -13,8 +11,44 @@ func main() {
 	fmt.Scanln(&number)
 	switch number {
 	case "1":
-		e := changed()
-		fmt.Println(e)
+		var number_changing string
+		fmt.Printf("1)changing to gregorian \n 2)changing to jalali \n ~chose:")
+		fmt.Scanln(&number_changing)
+		switch number_changing {
+		case "1":
+			var year_change int
+			var month_change int
+			var day_change int
+
+			fmt.Printf("write year:")
+			fmt.Scanln(&year_change)
+
+			fmt.Printf("write month:")
+			fmt.Scanln(&month_change)
+
+			fmt.Printf("write day:")
+			fmt.Scanln(&day_change)
+
+			e, r, t := changing.Changed_to_gregorian(year_change, month_change, day_change)
+			fmt.Println(e, r, t)
+		case "2":
+			var year_change int
+			var month_change int
+			var day_change int
+
+			fmt.Printf("write year:")
+			fmt.Scanln(&year_change)
+
+			fmt.Printf("write month:")
+			fmt.Scanln(&month_change)
+
+			fmt.Printf("write day:")
+			fmt.Scanln(&day_change)
+
+			e, r, t := changing.Changed_to_jalali(year_change, month_change, day_change)
+			fmt.Println(e, r, t)
+		}
+		
 	}
 
 }
